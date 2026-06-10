bf1bb9b5a5813c13788296a7cbd0632a5d61f5e2
Vedantsayare <vedantsayare25@gmail.com>
2026-06-11 01:23:06 +0530

    problem 8 done

---
+++ prac8.py
@@ -0,0 +1,7 @@
+#Write a program that takes a string and reverses it (e.g., “Python” becomes “nohtyP”).
+
+text = "Python"
+
+reversed = text[::-1]
+print(f"Original string: {text}")
+print(f"Reversed string: {reversed}")
