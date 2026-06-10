0ff0475c8fec460681420c7ce84b94bdc60dcfb4
Vedantsayare <vedantsayare25@gmail.com>
2026-06-11 01:17:33 +0530

    problem7 done

---
+++ prac7.py
@@ -0,0 +1,7 @@
+#Create a list of 5 fruits. Add a new fruit to the end of the list, then remove the second fruit (at index 1).
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+fruits.append("fig")
+fruits.pop(1)
+print(fruits)
