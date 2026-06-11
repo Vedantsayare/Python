0f3dfa024048ee8b737f6cb2014119f58bb998c9
Vedantsayare <vedantsayare25@gmail.com>
2026-06-11 16:01:01 +0530

    17 done

---
+++ prac17.py
@@ -0,0 +1,15 @@
+#Create a new list from two given lists such that the new list contains odd numbers from the first list and even numbers from the second list.
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+list3 = []
+
+for num in list1:
+    if num % 2 != 0:
+        list3.append(num)
+
+for num in list2:
+    if num % 2 ==0:
+        list3.append(num)
+
+print(list3)
